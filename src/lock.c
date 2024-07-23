d3102481321a2d7b319268e68cb7d2a9237a81a7
Turtel216 <papadimitrios216@gmail.com>
2024-07-23 14:23:02 +0300

    Added heap structes

--- src/lock.c
+++ src/lock.c
@@ -1,6 +1,21 @@
 #include "lock.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef enum e_heap_group { TINY, SMALL, LARGE } t_heap_group;
 
 typedef struct s_heap {
+	struct s_heap *prev;
+	struct s_heap *next;
+	t_heap_group group;
+	size_t size;
+	size_t free_size;
+	size_t count;
 } t_heap;
+
 typedef struct s_block {
+	struct s_block *prev;
+	struct s_block *next;
+	size_t size;
+	bool freed;
 } t_block;
