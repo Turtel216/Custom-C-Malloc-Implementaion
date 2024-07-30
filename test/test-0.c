3f74b7613c05429e8ebd3ec2f2cc689589bff3cb
Turtel216 <papadimitrios216@gmail.com>
2024-07-30 06:58:22 +0300

    Refactored test-0

--- test/test-0.c
+++ test/test-0.c
@@ -1,46 +1,35 @@
+#include <assert.h>
 #include <stdio.h>
 #include "../src/lock.h"
 
 int main()
 {
 	int *ptr = lock(sizeof(int));
-	if (ptr == NULL) {
-		printf("Failed to malloc a single int\n");
-		return 1;
-	}
+	assert(ptr != NULL && "Failed to malloc a single int");
 
 	*ptr = 1;
 	*ptr = 100;
 
 	za_hando(ptr);
 
-	printf("malloc'd an int, assigned to it, and free'd it\n");
-
 	int *ptr2 = lock(sizeof(int));
-	if (ptr2 == NULL) {
-		printf("Failed to malloc a single int\n");
-		return 1;
-	}
+	assert(ptr2 != NULL && "Failed to malloc a single int after free");
 
 	*ptr2 = 2;
 	*ptr2 = 200;
 
 	za_hando(ptr2);
-	printf("malloc'd an int, assigned to it, and free'd it #2\n");
 
 	lock(1); // Screw up alignment.
 
 	int *ptr3 = lock(sizeof(int));
-	if (ptr3 == NULL) {
-		printf("Failed to malloc a single int\n");
-		return 1;
-	}
+	assert(ptr3 != NULL && "Failed to malloc a single int after lock(1)");
 
 	*ptr3 = 3;
 	*ptr3 = 300;
 
 	za_hando(ptr3);
-	printf("malloc'd an int, assigned to it, and free'd it #3\n");
+	printf("Passed test-0");
 
 	return 0;
 }
